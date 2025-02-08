eba3c0ea63183d11722786df0942f07da141ab9e
tbreschi3 <34063350-tbreschi3@users.noreply.replit.com>
2025-02-08 08:29:39 +0000

    Restored to '94b4c3cdcb2655f1b6349d2b809d8e7bede1cd6a'
    Replit-Restored-To:94b4c3cdcb2655f1b6349d2b809d8e7bede1cd6a

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,22 +64,25 @@ export default {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-        grid: {
-          "0%": { transform: "translateY(-50%)" },
-          "100%": { transform: "translateY(0)" },
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "grid": "grid 15s linear infinite",
       },
     },
   },
