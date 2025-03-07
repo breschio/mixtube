8ac1deaee8d08546b6fa974f71d29150e5a21870
tbreschi3 <34063350-tbreschi3@users.noreply.replit.com>
2025-03-07 18:58:22 +0000

    Restored to '1a3c3670fe719fdca9de85f420511b54b5d0d63d'
    Replit-Restored-To: 1a3c3670fe719fdca9de85f420511b54b5d0d63d

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,7 @@ export default {
   darkMode: ["class"],
   content: [
     "./client/index.html",
-    "./client/src/**/*.{js,ts,jsx,tsx,css}",
-  ],
-  safelist: [
-    'animate-slow-pulse',
-    'mixtube-logo',
+    "./client/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -74,15 +70,10 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "slow-pulse": {
-          "0%, 100%": { opacity: "1" },
-          "50%": { opacity: "0.5" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "slow-pulse": "slow-pulse 3s ease-in-out infinite",
       },
     },
   },
